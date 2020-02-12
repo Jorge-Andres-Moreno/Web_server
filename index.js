9372979c762012f56f10b1e023e68cfb3118e951
Ubuntu <ubuntu@ip-172-31-36-45.us-east-2.compute.internal>
2020-02-12 23:25:21 +0000

    Initial commit, first part of deployment to backend

---
+++ index.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 8080;
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    console.log('GET="/"');
+    fs.readFile("./index.html", "UTF-8", function (err, html) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(html);
+    });  
+});
+
+app.post('/add', function (req, res) {
+    console.log('POST="/add"');
+    console.log(req.body)
+    fs.readFile("./index.html", "UTF-8", function (err, html) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
+      });
+});
+
+app.post('/list', function (req, res) {
+  console.log('GET="/list"');
+  fs.readFile("./index.html", "UTF-8", function (err, html) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(html);
+    });
+});
+
+http.createServer(app).listen(port);
